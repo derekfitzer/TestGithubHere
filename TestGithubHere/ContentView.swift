df60c063ed0227e9356c76df6ac21146198f5742
Derek Fitzer <franzderek@gmail.com>
2025-05-01 09:16:12 -0400

    Initial Commit

---
+++ TestGithubHere/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestGithubHere
+//
+//  Created by Derek Fitzer on 5/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestGithubHere/TestGithubHereApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGithubHereApp.swift
+//  TestGithubHere
+//
+//  Created by Derek Fitzer on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct TestGithubHereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
