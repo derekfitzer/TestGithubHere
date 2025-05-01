02d39ce543b9f0316a1ce3b9ef603cef7f2b9a8f
Joshua Moore <moorejoshua285@gmail.com>
2025-05-01 13:28:36 -0400

    Added JLM view

---
+++ TestGithubHere/JLMView.swift
@@ -0,0 +1,18 @@
+//
+//  JLMView.swift
+//  TestGithubHere
+//
+//  Created by Joshua Moore on 5/1/25.
+//
+
+import SwiftUI
+// Oop! Nothing to see here!
+struct JLMView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JLMView()
+}
