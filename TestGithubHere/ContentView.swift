22bc0994efd9e8f844f7b75fd7bd80740fff3e6e
Derek Fitzer <franzderek@gmail.com>
2025-05-01 12:25:52 -0400

    updatedpagers

--- TestGithubHere/ContentView.swift
+++ TestGithubHere/ContentView.swift
@@ -4,21 +4,35 @@
 //
 //  Created by Derek Fitzer on 5/1/25.
 //
+// Create a new swiftui view and add your name and a photo to the view. Add a link inside the navigation list below
+
+// name all files with your three initials ( for every file you add!
+
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            VStack {
+                List{
+                    NavigationLink {
+                        FDFView()
+                    } label: {
+                        Text("Mr. Fitzers Page")
+                    }
+                    NavigationLink {
+                        FDFView()
+                    } label: {
+                        Text("Go To Another Page")
+                    }
+
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
---
+++ TestGithubHere/FDFView.swift
@@ -0,0 +1,21 @@
+//
+//  FDFView.swift
+//  TestGithubHere
+//
+//  Created by Derek Fitzer on 5/1/25.
+//
+
+import SwiftUI
+
+struct FDFView: View {
+    var body: some View {
+        Image("FDFfitzerart")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+        Text("Welcome to Mr. Fitzer's Page")
+    }
+}
+
+#Preview {
+    FDFView()
+}
